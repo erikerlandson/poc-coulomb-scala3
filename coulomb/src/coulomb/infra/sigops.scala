55c75307eb5cc487dbc79505d594b59d6827d35f
Erik Erlandson <eerlands@redhat.com>
2021-03-20 09:36:22 -0700

    repro match type bug

--- coulomb/src/coulomb/infra/sigops.scala
+++ coulomb/src/coulomb/infra/sigops.scala
@@ -1,4 +1,24 @@
 package coulomb.infra
 
-// skeleton: just giving mill something to build
-trait EmptyTrait
+import scala.compiletime.ops.int._
+import scala.compiletime.ops.boolean._
+import scala.compiletime.ops.any._
+
+trait SNil
+trait %:[H, T]
+
+type FilterZ1[S] = S match
+    case SNil => SNil
+    case 0 %: t => t
+    case h %: t => h %: FilterZ1[t]
+
+// shim to enforce singleton type inference
+trait Shim[V <: Singleton]
+
+type FilterZ2[S] = S match
+    case SNil => SNil
+    case Shim[0] %: t => t
+    case Shim[h] %: t => h %: FilterZ2[t]
+
+trait Alloc[T]
+def alloc[T] = new Alloc[T] {}

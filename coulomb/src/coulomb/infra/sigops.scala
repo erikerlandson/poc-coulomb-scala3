9ff7806310e84d84be9579075e04bad247a1ca45
Erik Erlandson <eerlands@redhat.com>
2021-03-21 11:37:46 -0700

    demo Quantity as opaque type

--- coulomb/src/coulomb/infra/sigops.scala
+++ coulomb/src/coulomb/infra/sigops.scala
@@ -22,3 +22,26 @@ type FilterZ2[S] = S match
 
 trait Alloc[T]
 def alloc[T] = new Alloc[T] {}
+
+trait Addable[V]:
+    def plus(x: V, y: V): V
+  
+given Addable[Int] with
+    def plus(x: Int, y: Int): Int = x + y
+
+trait Second
+
+object demo:
+    opaque type Quantity[V, U] = V
+
+    object Quantity:
+    end Quantity
+
+    extension[V] (v: V)
+      def withUnit[U]: Quantity[V, U] = v
+
+    extension[V, U] (ql: Quantity[V, U])
+        def value: V = ql
+        def +(qr: Quantity[V, U])(using va: Addable[V]): Quantity[V, U] =
+            va.plus(ql, qr)
+end demo

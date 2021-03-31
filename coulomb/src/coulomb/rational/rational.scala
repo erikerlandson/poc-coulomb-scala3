f14ca73efd66d3db349ba8e907af90a45b067e1b
Erik Erlandson <eerlands@redhat.com>
2021-03-31 14:09:50 -0700

    factor typelevel rational to new file

--- coulomb/src/coulomb/rational/rational.scala
+++ coulomb/src/coulomb/rational/rational.scala
@@ -1,192 +1,101 @@
-package coulomb.infra
+package coulomb.rational
 
-import scala.quoted.*
-import scala.annotation.{ targetName, implicitNotFound }
+class Rational private (val n: BigInt, val d: BigInt) extends Serializable:
+    import Rational.canonical
 
-object rational:
-    class Rational private (val n: BigInt, val d: BigInt) extends Serializable:
-        import Rational.canonical
+    override def toString: String =
+        if (d == 1) s"$n" else s"$n/$d"
 
-        override def toString: String =
-            if (d == 1) s"$n" else s"$n/$d"
+    inline def +(rhs: Rational): Rational =
+        canonical((n * rhs.d) + (rhs.n * d), d * rhs.d)
 
-        inline def +(rhs: Rational): Rational =
-            canonical((n * rhs.d) + (rhs.n * d), d * rhs.d)
+    inline def -(rhs: Rational): Rational =
+        canonical((n * rhs.d) - (rhs.n * d), d * rhs.d)
 
-        inline def -(rhs: Rational): Rational =
-            canonical((n * rhs.d) - (rhs.n * d), d * rhs.d)
+    inline def *(rhs: Rational): Rational =
+        canonical(n * rhs.n, d * rhs.d)
 
-        inline def *(rhs: Rational): Rational =
-            canonical(n * rhs.n, d * rhs.d)
+    inline def /(rhs: Rational): Rational =
+        canonical(n * rhs.d, d * rhs.n)
 
-        inline def /(rhs: Rational): Rational =
-            canonical(n * rhs.d, d * rhs.n)
+    inline def unary_- : Rational =
+        canonical(-n, d)
 
-        inline def unary_- : Rational =
-            canonical(-n, d)
-
-        def pow(e: Rational): Rational =
-            if (n < 0) then
-                require(e.d == 1) // negative can only be raised to integer power
-                val p = (-this).pow(e)
-                if (e.n % 2 == 0) then p else -p
-            else if (e.n < 0) then
-                canonical(d, n).pow(-e)
-            else if (e.n == 0) then
-                require(n > 0) // 0.pow(0) is undefined
-                canonical(1, 1)
-            else if (e.n == 1) then
-                if (e.d == 1) then this else
-                    // this >= 0, e is of form 1/k
-                    (Rational.integerRoot(n, e.d), Rational.integerRoot(d, e.d)) match
-                        case (Some(nr), Some(dr)) => canonical(nr, dr)
-                        case _ => Rational(scala.math.pow(this.toDouble, 1.0 / e.d.toDouble))
+    def pow(e: Rational): Rational =
+        if (n < 0) then
+            require(e.d == 1) // negative can only be raised to integer power
+            val p = (-this).pow(e)
+            if (e.n % 2 == 0) then p else -p
+        else if (e.n < 0) then
+            canonical(d, n).pow(-e)
+        else if (e.n == 0) then
+            require(n > 0) // 0.pow(0) is undefined
+            canonical(1, 1)
+        else if (e.n == 1) then
+            if (e.d == 1) then this else
+                // this >= 0, e is of form 1/k
+                (Rational.integerRoot(n, e.d), Rational.integerRoot(d, e.d)) match
+                    case (Some(nr), Some(dr)) => canonical(nr, dr)
+                    case _ => Rational(scala.math.pow(this.toDouble, 1.0 / e.d.toDouble))
+        else
+            if (e.d == 1) then
+                canonical(n.pow(e.n.toInt), d.pow(e.n.toInt))
             else
-                if (e.d == 1) then
-                    canonical(n.pow(e.n.toInt), d.pow(e.n.toInt))
-                else
-                    val p = canonical(e.n, 1)
-                    val r = canonical(1, e.d)
-                    this.pow(p).pow(r)
+                val p = canonical(e.n, 1)
+                val r = canonical(1, e.d)
+                this.pow(p).pow(r)
 
-    end Rational
+end Rational
 
-    object Rational:
-        import scala.math.*
+object Rational:
+    import scala.math.*
 
-        inline def apply(n: BigInt, d: BigInt): Rational = canonical(n, d)
+    inline def apply(n: BigInt, d: BigInt): Rational = canonical(n, d)
 
-        inline def apply(r: Rational): Rational = canonical(r.n, r.d)
+    inline def apply(r: Rational): Rational = canonical(r.n, r.d)
 
-        inline def apply(v: Int): Rational = canonical(v, 1)
-        inline def apply(v: Long): Rational = canonical(v, 1)
-        inline def apply(v: Float): Rational = apply(v.toDouble)
+    inline def apply(v: Int): Rational = canonical(v, 1)
+    inline def apply(v: Long): Rational = canonical(v, 1)
+    inline def apply(v: Float): Rational = apply(v.toDouble)
 
-        def apply(v: Double): Rational =
-            if (abs(v) == 0.0) then canonical(0, 1)
-            else
-                // IEEE double precision guaranteed 15 base-10 digits of precision
-                val e = 15 - (floor(log10(abs(v))).toInt + 1)
-                val (np10, dp10) = if (e < 0) then (-e, 0) else (0, e)
-                val vi = v * pow(10, e)
-                val n = BigInt(vi.toLong) * BigInt(10).pow(np10)
-                val d = BigInt(10).pow(dp10)
-                canonical(n, d)
+    def apply(v: Double): Rational =
+        if (abs(v) == 0.0) then canonical(0, 1)
+        else
+            // IEEE double precision guaranteed 15 base-10 digits of precision
+            val e = 15 - (floor(log10(abs(v))).toInt + 1)
+            val (np10, dp10) = if (e < 0) then (-e, 0) else (0, e)
+            val vi = v * pow(10, e)
+            val n = BigInt(vi.toLong) * BigInt(10).pow(np10)
+            val d = BigInt(10).pow(dp10)
+            canonical(n, d)
 
-        // intended to be the single safe way to construct a canonical rational
-        // every construction of a new Rational should reduce to some call to this method
-        def canonical(n: BigInt, d: BigInt): Rational =
-            require(d != 0, "Rational denominator cannot be zero")
-            if (n == 0)
-                // canonical zero is 0/1
-                new Rational(0, 1)
-            else if (d < 0) then
-                // canonical denominator is always positive
-                canonical(-n, -d)
-            else
-                // canonical rationals are fully reduced
-                val g = n.gcd(d)
-                new Rational(n / g, d / g)
+    // intended to be the single safe way to construct a canonical rational
+    // every construction of a new Rational should reduce to some call to this method
+    def canonical(n: BigInt, d: BigInt): Rational =
+        require(d != 0, "Rational denominator cannot be zero")
+        if (n == 0)
+            // canonical zero is 0/1
+            new Rational(0, 1)
+        else if (d < 0) then
+            // canonical denominator is always positive
+            canonical(-n, -d)
+        else
+            // canonical rationals are fully reduced
+            val g = n.gcd(d)
+            new Rational(n / g, d / g)
 
-        def integerRoot(v: BigInt, k: BigInt): Option[BigInt] =
-            require(v >= 0 && k > 0)
-            if k == 1 then Some(v)
-            else
-                val t = if k == 2 then sqrt(v.toDouble)
-                else if k == 3 then cbrt(v.toDouble)
-                else pow(v.toDouble, 1.0 / k.toDouble)
-                if (t == rint(t)) then Some(BigInt(t.toLong)) else None
-    end Rational
+    private [rational] def integerRoot(v: BigInt, k: BigInt): Option[BigInt] =
+        require(v >= 0 && k > 0)
+        if k == 1 then Some(v)
+        else
+            val t = if k == 2 then sqrt(v.toDouble)
+            else if k == 3 then cbrt(v.toDouble)
+            else pow(v.toDouble, 1.0 / k.toDouble)
+            if (t == rint(t)) then Some(BigInt(t.toLong)) else None
+end Rational
 
-    extension(r: Rational)
-        inline def toInt: Int = r.toDouble.toInt
-        inline def toLong: Long = r.toDouble.toLong
-        inline def toFloat: Float = r.toDouble.toFloat
-        inline def toDouble: Double = r.n.toDouble / r.d.toDouble
-
-    // rational exponents at the type level
-    trait /%[L, R]
-
-    @implicitNotFound("${L} /%+ ${R} has no defined context")
-    trait /%+[L, R]:
-        type Res
-    object /%+ :
-        transparent inline given [L, R]: /%+[L, R] = ${ macros.addImpl[L, R] }
-
-    trait /%-[L, R]:
-        type Res
-    object /%- :
-        transparent inline given [L, R]: /%-[L, R] = ${ macros.subImpl[L, R] }
-
-    trait /%*[L, R]:
-        type Res
-    object /%* :
-        transparent inline given [L, R]: /%*[L, R] = ${ macros.mulImpl[L, R] }
-
-    trait /%/[L, R]:
-        type Res
-    object /%/ :
-        transparent inline given [L, R]: /%/[L, R] = ${ macros.divImpl[L, R] }
-
-    object macros:
-        import scala.quoted.*
-
-        def addImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%+[L, R]] =
-            resType(resVal[L, R](_ + _)) match
-                case '[resT] => '{ new _root_.coulomb.infra.rational./%+[L, R] { type Res = resT } }
-
-        def subImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%-[L, R]] =
-            resType(resVal[L, R](_ - _)) match
-                case '[resT] => '{ new _root_.coulomb.infra.rational./%-[L, R] { type Res = resT } }
-
-        def mulImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%*[L, R]] =
-            resType(resVal[L, R](_ * _)) match
-                case '[resT] => '{ new _root_.coulomb.infra.rational./%*[L, R] { type Res = resT } }
-
-        def divImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%/[L, R]] =
-            resType(resVal[L, R](_ / _)) match
-                case '[resT] => '{ new _root_.coulomb.infra.rational./%/[L, R] { type Res = resT } }
-
-        def resVal[L, R](op: (Rational, Rational) => Rational)(using Type[L], Type[R], Quotes): Rational =
-            import quotes.reflect.*
-            (TypeRepr.of[L], TypeRepr.of[R]) match
-                case (IntLiteralType(lv), IntLiteralType(rv)) => op(Rational(lv, 1), Rational(rv, 1))
-                case (RationalType(ln, ld), RationalType(rn, rd)) => op(Rational(ln, ld), Rational(rn, rd))
-                case (IntLiteralType(lv), RationalType(rn, rd)) => op(Rational(lv, 1), Rational(rn, rd))
-                case (RationalType(ln, ld), IntLiteralType(rv)) => op(Rational(ln, ld), Rational(rv, 1))
-                case _ => { report.error("Unsupported types for Rational function"); Rational(0, 1) }
-
-        def resType(res: Rational)(using Quotes): Type[?] =
-            import quotes.reflect.*
-            val tres: TypeRepr = if (res.d == 1) then
-                ConstantType(IntConstant(res.n.toInt))
-            else
-                val resTC = TypeRepr.of[_root_.coulomb.infra.rational./%]
-                resTC.appliedTo(List(ConstantType(IntConstant(res.n.toInt)), ConstantType(IntConstant(res.d.toInt))))
-            tres.asType
-
-        object IntLiteralType:
-            def unapply(tr: Any)(using Quotes): Option[Int] =
-                 import quotes.reflect.*
-                 try
-                     // I haven't figured out a way to specify TypeRepr for parameter 'tr'.
-                     // Wrapping this in try/catch adequately fakes type soundness here, since if it fails,
-                     // the correct answer is None anyway
-                     tr.asInstanceOf[TypeRepr] match
-                        case ConstantType(IntConstant(i)) => Some(i)
-                        case _ => None
-                 catch _ => None
-
-        object RationalType:
-            def unapply(tr: Any)(using Quotes): Option[(Int, Int)] =
-                 import quotes.reflect.*
-                 try
-                     tr.asInstanceOf[TypeRepr] match
-                        case AppliedType(tc, List(ConstantType(IntConstant(n)), ConstantType(IntConstant(d)))) if (tc.typeSymbol.name == "/%") =>
-                            Some((n, d))
-                        case _ => None
-                 catch _ => None
-
-    end macros
-
-end rational
+extension(r: Rational)
+    inline def toInt: Int = r.toDouble.toInt
+    inline def toLong: Long = r.toDouble.toLong
+    inline def toFloat: Float = r.toDouble.toFloat
+    inline def toDouble: Double = r.n.toDouble / r.d.toDouble

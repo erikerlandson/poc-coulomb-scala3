88a020f744c67fe7764570305e13fbd5996caa85
Erik Erlandson <eerlands@redhat.com>
2021-03-31 16:50:37 -0700

    remove match type test

--- coulomb/src/coulomb/infra/sigops.scala
+++ coulomb/src/coulomb/infra/sigops.scala
@@ -7,19 +7,6 @@ import scala.compiletime.ops.any.*
 trait SNil
 trait %:[H, T]
 
-type FilterZ1[S] = S match
-    case SNil => SNil
-    case 0 %: t => t
-    case h %: t => h %: FilterZ1[t]
-
-// shim to enforce singleton type inference
-trait Shim[V <: Singleton]
-
-type FilterZ2[S] = S match
-    case SNil => SNil
-    case Shim[0] %: t => t
-    case Shim[h] %: t => h %: FilterZ2[t]
-
 trait Alloc[T]
 def alloc[T] = new Alloc[T] {}
 

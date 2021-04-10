32a59b2c7cbe877196a7cb307a30dd313557a81e
Erik Erlandson <eerlands@redhat.com>
2021-04-10 11:58:17 -0700

    add intermediat layer to bridge macro and Eval/Aux

--- coulomb/src/coulomb/rational/typelevel.scala
+++ coulomb/src/coulomb/rational/typelevel.scala
@@ -9,44 +9,80 @@ trait /%[L, R]
 trait /%+[L, R]:
     type Res
 object /%+ :
-    transparent inline given [L, R]: /%+[L, R] = ${ macros.addImpl[L, R] }
+    type Eval[L, R, X] = /%+[L, R] { type Res = X }
+    given [L, R](using x: AddRA[L, R]): Eval[L, R, x.Res] =
+        new /%+[L, R] { type Res = x.Res }
 
 @implicitNotFound("Typelevel rational subtraction undefined for: ${L} /%- ${R}")
 trait /%-[L, R]:
     type Res
 object /%- :
-    transparent inline given [L, R]: /%-[L, R] = ${ macros.subImpl[L, R] }
+    type Eval[L, R, X] = /%-[L, R] { type Res = X }
+    given [L, R](using x: SubRA[L, R]): Eval[L, R, x.Res] =
+        new /%-[L, R] { type Res = x.Res }
 
 @implicitNotFound("Typelevel rational multiplication undefined for: ${L} /%* ${R}")
 trait /%*[L, R]:
     type Res
 object /%* :
-    transparent inline given [L, R]: /%*[L, R] = ${ macros.mulImpl[L, R] }
+    type Eval[L, R, X] = /%*[L, R] { type Res = X }
+    given [L, R](using x: MulRA[L, R]): Eval[L, R, x.Res] =
+        new /%*[L, R] { type Res = x.Res }
 
 @implicitNotFound("Typelevel rational division undefined for: ${L} /%/ ${R}")
 trait /%/[L, R]:
     type Res
 object /%/ :
-    transparent inline given [L, R]: /%/[L, R] = ${ macros.divImpl[L, R] }
+    type Eval[L, R, X] = /%/[L, R] { type Res = X }
+    given [L, R](using x: DivRA[L, R]): Eval[L, R, x.Res] =
+        new /%/[L, R] { type Res = x.Res }
+
+// Chained implicits for unit signature operations do not operate correctly
+// with pure dependent-type: I need the Eval (aka Aux) types, however
+// The macros will not operate correctly and also support Eval, and so
+// I have this intermediate layer to bridge the two.
+@implicitNotFound("Typelevel rational addition undefined for: AddRA[${L}, ${R}]")
+trait AddRA[L, R]:
+    type Res
+object AddRA :
+    transparent inline given [L, R]: AddRA[L, R] = ${ macros.addImpl[L, R] }
+
+@implicitNotFound("Typelevel rational subtraction undefined for: SubRA[${L}, ${R}]")
+trait SubRA[L, R]:
+    type Res
+object SubRA :
+    transparent inline given [L, R]: SubRA[L, R] = ${ macros.subImpl[L, R] }
+
+@implicitNotFound("Typelevel rational multiplication undefined for: MulRA[${L}, ${R}]")
+trait MulRA[L, R]:
+    type Res
+object MulRA :
+    transparent inline given [L, R]: MulRA[L, R] = ${ macros.mulImpl[L, R] }
+
+@implicitNotFound("Typelevel rational division undefined for: DivRA[${L}, ${R}]")
+trait DivRA[L, R]:
+    type Res
+object DivRA :
+    transparent inline given [L, R]: DivRA[L, R] = ${ macros.divImpl[L, R] }
 
 object macros:
     import scala.quoted.*
 
-    def addImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%+[L, R]] =
+    def addImpl[L, R](using Type[L], Type[R], Quotes): Expr[AddRA[L, R]] =
         resType(resVal[L, R](_ + _)) match
-            case '[resT] => '{ new _root_.coulomb.rational./%+[L, R] { type Res = resT } }
+            case '[resT] => '{ new _root_.coulomb.rational.AddRA[L, R] { type Res = resT } }
 
-    def subImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%-[L, R]] =
+    def subImpl[L, R](using Type[L], Type[R], Quotes): Expr[SubRA[L, R]] =
         resType(resVal[L, R](_ - _)) match
-            case '[resT] => '{ new _root_.coulomb.rational./%-[L, R] { type Res = resT } }
+            case '[resT] => '{ new _root_.coulomb.rational.SubRA[L, R] { type Res = resT } }
 
-    def mulImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%*[L, R]] =
+    def mulImpl[L, R](using Type[L], Type[R], Quotes): Expr[MulRA[L, R]] =
         resType(resVal[L, R](_ * _)) match
-            case '[resT] => '{ new _root_.coulomb.rational./%*[L, R] { type Res = resT } }
+            case '[resT] => '{ new _root_.coulomb.rational.MulRA[L, R] { type Res = resT } }
 
-    def divImpl[L, R](using Type[L], Type[R], Quotes): Expr[/%/[L, R]] =
+    def divImpl[L, R](using Type[L], Type[R], Quotes): Expr[DivRA[L, R]] =
         resType(resVal[L, R](_ / _)) match
-            case '[resT] => '{ new _root_.coulomb.rational./%/[L, R] { type Res = resT } }
+            case '[resT] => '{ new _root_.coulomb.rational.DivRA[L, R] { type Res = resT } }
 
     def resVal[L, R](op: (Rational, Rational) => Rational)(using Type[L], Type[R], Quotes): Rational =
         import quotes.reflect.*

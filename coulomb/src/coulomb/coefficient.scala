22afaa62cc4a8a465896c210a287aac92d7bbe1f
Erik Erlandson <eerlands@redhat.com>
2021-04-17 15:59:05 -0700

    functional stub for Coefficient

--- coulomb/src/coulomb/coefficient.scala
+++ coulomb/src/coulomb/coefficient.scala
@@ -3,11 +3,11 @@ package coulomb
 import coulomb.rational.Rational
 
 trait Coefficient[U1, U2]:
-    def coef: Rational
+    val coef: Rational
 object Coefficient:
     import scala.quoted.*
 
     inline given [U1, U2]: Coefficient[U1, U2] = $ { coefMeta[U1, U2] }
 
     def coefMeta[U1, U2](using Type[U1], Type[U2], Quotes): Expr[Coefficient[U1, U2]] =
-        '{ ??? }
+        '{ new Coefficient[U1, U2] { val coef = Rational(1) } }

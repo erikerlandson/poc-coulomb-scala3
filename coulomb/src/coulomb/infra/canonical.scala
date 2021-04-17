8349bd70495bafc4b466bc9fb8ae1f5e280461b4
Erik Erlandson <eerlands@redhat.com>
2021-04-17 15:36:15 -0700

    stub CanonicalSig[U]

---
+++ coulomb/src/coulomb/infra/canonical.scala
@@ -0,0 +1,8 @@
+package coulomb.infra
+
+// possibly will not need this, if meta-programming implementation
+// proves out
+trait CanonicalSig[U]:
+    type Res
+object CanonicalSig:
+end CanonicalSig

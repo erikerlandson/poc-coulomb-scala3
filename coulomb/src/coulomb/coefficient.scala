9dd38d505e55da2c39c3a7d60f7748f231673df4
Erik Erlandson <eerlands@redhat.com>
2021-04-17 15:52:14 -0700

    stub Coefficient[U1, U2]

---
+++ coulomb/src/coulomb/coefficient.scala
@@ -0,0 +1,13 @@
+package coulomb
+
+import coulomb.rational.Rational
+
+trait Coefficient[U1, U2]:
+    def coef: Rational
+object Coefficient:
+    import scala.quoted.*
+
+    inline given [U1, U2]: Coefficient[U1, U2] = $ { coefMeta[U1, U2] }
+
+    def coefMeta[U1, U2](using Type[U1], Type[U2], Quotes): Expr[Coefficient[U1, U2]] =
+        '{ ??? }

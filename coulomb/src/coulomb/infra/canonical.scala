015734298c556e3c5f8b4c1bff2bd01c578a0f44
Erik Erlandson <eerlands@redhat.com>
2021-04-28 14:19:20 -0700

    part of canonical

--- coulomb/src/coulomb/infra/canonical.scala
+++ coulomb/src/coulomb/infra/canonical.scala
@@ -1,8 +1,30 @@
 package coulomb.infra
 
-// possibly will not need this, if meta-programming implementation
-// proves out
+import scala.quoted.*
+
+import coulomb.rational.Rational
+import coulomb.rational.{ /%, /%+, /%-, /%* }
+import coulomb.{ %*, %/, %^ }
+import coulomb.define.*
+
 trait CanonicalSig[U]:
     type Res
+    val coef: Rational
+
 object CanonicalSig:
-end CanonicalSig
+    transparent inline given CanonicalSig[1] =
+        new CanonicalSig[1]:
+            type Res = SNil
+            val coef = Rational(1)
+
+    transparent inline given [U](using BaseUnit[U]): CanonicalSig[U] =
+        new CanonicalSig[U]:
+            type Res = (U, 1) %: SNil
+            val coef = Rational(1)
+
+    transparent inline given [U, D](using
+            du: DerivedUnit[U, D],
+            ds: CanonicalSig[D]): CanonicalSig[U] =
+        new CanonicalSig[U]:
+            type Res = ds.Res
+            val coef = du.coef * ds.coef

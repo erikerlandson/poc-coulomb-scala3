ba420e3f498be2f0794f42436498c76b2aa746bd
Erik Erlandson <eerlands@redhat.com>
2021-04-28 14:18:58 -0700

    context based

--- coulomb/src/coulomb/coefficient.scala
+++ coulomb/src/coulomb/coefficient.scala
@@ -1,13 +1,13 @@
 package coulomb
 
 import coulomb.rational.Rational
+import coulomb.infra.{ CanonicalSig, SNil }
 
 trait Coefficient[U1, U2]:
     val coef: Rational
 object Coefficient:
-    import scala.quoted.*
-
-    inline given [U1, U2]: Coefficient[U1, U2] = $ { coefMeta[U1, U2] }
-
-    def coefMeta[U1, U2](using Type[U1], Type[U2], Quotes): Expr[Coefficient[U1, U2]] =
-        '{ new Coefficient[U1, U2] { val coef = Rational(1) } }
+    inline given [U1, U2](using
+        sig: CanonicalSig[U1 %/ U2],
+        sig1: sig.Res =:= SNil):
+            Coefficient[U1, U2] =
+        new Coefficient[U1, U2] { val coef = sig.coef }

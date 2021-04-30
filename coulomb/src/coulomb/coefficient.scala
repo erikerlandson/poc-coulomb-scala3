b9eab4efda7e49913334f626624f02134d43d023
Erik Erlandson <eerlands@redhat.com>
2021-04-30 13:59:47 -0700

    add optimized rule for Coefficient[U, U]

--- coulomb/src/coulomb/coefficient.scala
+++ coulomb/src/coulomb/coefficient.scala
@@ -6,6 +6,9 @@ import coulomb.infra.{ CanonicalSig, SNil }
 trait Coefficient[U1, U2]:
     val coef: Rational
 object Coefficient:
+    inline given [U]: Coefficient[U, U] =
+        new Coefficient[U, U] { val coef = Rational.const1 }
+
     inline given [U1, U2](using
         sig: CanonicalSig[U1 %/ U2],
         sig1: sig.Res =:= SNil):

4bf7c36944be7a176b8dbbe420e63f2057eb88e7
Erik Erlandson <eerlands@redhat.com>
2021-06-02 16:49:07 -0700

    refactor ops

--- coulomb/src/coulomb/ops/ops.scala
+++ coulomb/src/coulomb/ops/ops.scala
@@ -3,31 +3,9 @@ package coulomb.ops
 import scala.annotation.implicitNotFound
 
 import coulomb.*
-import coulomb.rational.Rational
 
 @implicitNotFound("Addition not defined in scope for Quantity[${VL}, ${UL}] and Quantity[${VR}, ${UR}]")
 abstract class Add[VL, UL, VR, UR]:
     type VO
     type UO
     def apply(vl: VL, vr: VR): VO
-
-object standard:
-    transparent inline given ua01[U]: Add[Double, U, Double, U] =
-        new Add[Double, U, Double, U]:
-            type VO = Double
-            type UO = U
-            def apply(vl: Double, vr: Double): Double = vl + vr
-
-    transparent inline given ua01[UL, UR](using conv: Coefficient[UR, UL]): Add[Double, UL, Double, UR] =
-        new Add[Double, UL, Double, UR]:
-            type VO = Double
-            type UO = UL
-            val c = conv.coef.toDouble
-            def apply(vl: Double, vr: Double): Double = vl + (c * vr)
-
-    transparent inline given ua02[UL, UR](using conv: Coefficient[UR, UL]): Add[Int, UL, Double, UR] =
-        new Add[Int, UL, Double, UR]:
-            type VO = Double
-            type UO = UL
-            val c = conv.coef.toDouble
-            def apply(vl: Int, vr: Double): Double = vl + (c * vr)
---
+++ coulomb/src/coulomb/ops/standard.scala
@@ -0,0 +1,24 @@
+package coulomb.ops.standard
+
+import coulomb.*
+import coulomb.ops.*
+
+transparent inline given g1[U]: Add[Double, U, Double, U] =
+    new Add[Double, U, Double, U]:
+        type VO = Double
+        type UO = U
+        def apply(vl: Double, vr: Double): Double = vl + vr
+
+transparent inline given g2[UL, UR](using conv: Coefficient[UR, UL]): Add[Double, UL, Double, UR] =
+    new Add[Double, UL, Double, UR]:
+        type VO = Double
+        type UO = UL
+        val c = conv.coef.toDouble
+        def apply(vl: Double, vr: Double): Double = vl + (c * vr)
+
+transparent inline given g3[UL, UR](using conv: Coefficient[UR, UL]): Add[Int, UL, Double, UR] =
+    new Add[Int, UL, Double, UR]:
+        type VO = Double
+        type UO = UL
+        val c = conv.coef.toDouble
+        def apply(vl: Int, vr: Double): Double = vl + (c * vr)
